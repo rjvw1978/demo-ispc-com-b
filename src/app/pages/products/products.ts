32b5b32138a7603975d64c866c05e5982c705c67
Rojas Córsico Ivana Soledad <irojas@gmail.com>
2026-09-14 20:18:04 -0300

    change detector + nginit

--- src/app/pages/products/products.ts
+++ src/app/pages/products/products.ts
@@ -7,21 +7,24 @@ import { ProductService } from '../../services/product-service';
   templateUrl: './products.html',
   styleUrl: './products.css',
 })
-export class Products {
+export class Products implements OnInit {
   gallery:any[] = [];
 
   constructor(private productService:ProductService, private cdr: ChangeDetectorRef){
 
-  this.productService.obtenerListaProductos().subscribe({
-    next: (data) => 
-      { console.log(data);
-        this.gallery = data;
-      },
-    error: (e) => console.error(e),
-    complete: () => 
-      { this.cdr.detectChanges();
-        console.info('complete');} 
-  });
+  }
+
+  ngOnInit(): void {
+     this.productService.obtenerListaProductos().subscribe({
+      next: (data) => 
+        { console.log(data);
+          this.gallery = data;
+        },
+      error: (e) => console.error(e),
+      complete: () => 
+        { this.cdr.detectChanges();
+          console.info('complete');} 
+    });
   }
     
   }
